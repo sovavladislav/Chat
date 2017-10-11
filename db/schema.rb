28a1fb1bb0bb681c0d3c2233467fab59eddf3abd
Vlad@MBP-Vlad <vladislav.sova@lakehouse.ru>
2017-10-11 17:11:37 +0300

    Change schema.rb file

--- db/schema.rb
+++ db/schema.rb
@@ -10,32 +10,34 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170825120456) do
+ActiveRecord::Schema.define(version: 20171009232915) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "conversations", force: :cascade do |t|
     t.string   "dialogName"
     t.integer  "user_id"
     t.integer  "theme_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["theme_id"], name: "index_conversations_on_theme_id"
-    t.index ["user_id"], name: "index_conversations_on_user_id"
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+    t.integer  "type_of_conversation_id"
+    t.index ["theme_id"], name: "index_conversations_on_theme_id", using: :btree
+    t.index ["user_id"], name: "index_conversations_on_user_id", using: :btree
   end
 
   create_table "groups", force: :cascade do |t|
     t.string   "name"
-    t.integer  "curator_id"
+    t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "invites", force: :cascade do |t|
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-    t.integer  "user_id"
-    t.integer  "conversation_id"
-    t.index ["conversation_id"], name: "index_invites_on_conversation_id"
-    t.index ["user_id"], name: "index_invites_on_user_id"
+    t.integer "user_id"
+    t.integer "conversation_id"
+    t.index ["conversation_id"], name: "index_invites_on_conversation_id", using: :btree
+    t.index ["user_id"], name: "index_invites_on_user_id", using: :btree
   end
 
   create_table "messages", force: :cascade do |t|
@@ -44,15 +46,23 @@ ActiveRecord::Schema.define(version: 20170825120456) do
     t.integer  "conversation_id"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
-    t.index ["conversation_id"], name: "index_messages_on_conversation_id"
-    t.index ["user_id"], name: "index_messages_on_user_id"
+    t.index ["conversation_id"], name: "index_messages_on_conversation_id", using: :btree
+    t.index ["user_id"], name: "index_messages_on_user_id", using: :btree
   end
 
   create_table "themes", force: :cascade do |t|
-    t.string   "themeName"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer  "type_id"
+    t.string   "name"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+    t.integer  "type_of_theme_id"
+  end
+
+  create_table "type_of_conversations", force: :cascade do |t|
+    t.string   "name"
+    t.string   "main_role"
+    t.string   "available_roles", default: [],              array: true
+    t.datetime "created_at",                   null: false
+    t.datetime "updated_at",                   null: false
   end
 
   create_table "type_of_themes", force: :cascade do |t|
@@ -84,8 +94,14 @@ ActiveRecord::Schema.define(version: 20170825120456) do
     t.string   "telephone_number"
     t.date     "year_of_st_ed"
     t.integer  "group_id"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "conversations", "themes"
+  add_foreign_key "conversations", "users"
+  add_foreign_key "invites", "conversations"
+  add_foreign_key "invites", "users"
+  add_foreign_key "messages", "conversations"
+  add_foreign_key "messages", "users"
 end
